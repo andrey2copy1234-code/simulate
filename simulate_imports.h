20be66b81f77f0947e7d038ad797c1fafac97e94
andrey2copy1234-code <andrey2copy1234@gmail.com>
2026-03-15 12:17:51 +0300

    добавлен файл импортов

---
+++ simulate_imports.h
@@ -0,0 +1,14 @@
+// cd C:/Users/Azerty/Desktop/Программы/filescpp; g++ simulate_imports.h -c -O3 -I c:\Users\Azerty\Downloads\SFML-2.6.1/include
+#include <SFML/Graphics.hpp>
+#include <windows.h>
+#include <commdlg.h>
+#include <fstream>
+#include <optional>
+#include <locale>
+#include <codecvt>
+#include <vector>
+#include <cmath>
+#include <random>
+#include <limits>
+#include <iostream>
+#include <mutex>
